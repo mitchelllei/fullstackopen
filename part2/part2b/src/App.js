80f8988e21f0c23c1052cb1000c85e613fb0de42
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-07-07 23:41:06 +0300

    add numbers to phonebook too

--- part2/part2b/src/App.js
+++ part2/part2b/src/App.js
@@ -2,30 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'John', id: 1 }
+    { name: 'John', number: "123", id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState('')
+ 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
    const inPhonebook = persons.some(item => item.name === newName) // true
-    if (inPhonebook === false) {
+   const inPhonebookNumber = persons.some(item => item.nnumber === newNumber)
+    if (inPhonebook === false & inPhonebookNumber === false) {
       console.log('name not in phonebook')
       setPersons(persons.concat(nameObject))
       setNewName('')
     } else {
-      alert(`${newName} is already in phonebook`)
+      alert(`name: ${newName} with number ${newNumber} is already in phonebook`)
     }
     
   }
+  
 
   return (
     <div>
@@ -36,6 +45,12 @@ const App = () => {
             value={newName}
             onChange={handleNameChange}
           />
+        </div>
+        <div>
+          number: <input
+          value = {newNumber}
+          onChange={handleNumberChange}
+           />
         </div>
         <div>
           <button type="submit">add</button>
