21df788b7ca6d87e010ee7a5de54f8a8d9058812
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-07-15 10:27:43 +0300

    mapping error

--- part2/part2_countries/src/components/Search.js
+++ part2/part2_countries/src/components/Search.js
@@ -1,48 +1,57 @@
 import React from "react";
+import axios from "axios";
 
 const Search = ({ countries, searchValue }) => {
   const countriesFiltered = countries.filter((country) =>
     country.name.common.toLowerCase().includes(searchValue.toLowerCase())
   );
-  console.log("aaa", countries[0])
-  const listedCountries =  countriesFiltered.map((countries) =>
-  <li key={countries.name.common}>{countries.name.common}</li>
-  )
-  if (searchValue === ""){
-    return <ul>{listedCountries}</ul>
-  }
-  if  (listedCountries.length >10){
-  return( <>
 
-  <p>Too many matches, make entry more specific</p>
-  <ul>{listedCountries}</ul>
-  </>
-  )
-  }
+  const listedCountries = countriesFiltered.map((countries) => (
+    <React.Fragment key={countries.name.common}>
+      <li> {countries.name.common} </li>
+    </React.Fragment>
+  ));
 
-  if (listedCountries.length === 1) {
-    return <>
-      <p>Here is the data for </p>
+  const listedCountriesData = countriesFiltered.map((countries) => (
+    <React.Fragment key={countries.name.common}>
+      <li> {countries.name.common}</li>
+      <li> capital {countries.capital}</li>
+      <li> area {countries.area}</li>
+      <img src={countries.flags.png} alt="Flag" />
+      
+      <li>
+        {
+         
+          countries.languages.map(({language,index}) => (
+          <React.Fragment key={index}>
+            <li>{language}</li>
+          </React.Fragment>
+        ))}
+      </li>
+    </React.Fragment>
+  ));
+      
+if (searchValue === "") {
+  return <ul>{listedCountries}</ul>;
+}
+if (listedCountries.length > 10) {
+  return (
+    <>
+      <p>Too many matches, make entry more specific</p>
       <ul>{listedCountries}</ul>
     </>
-  }
-  
+  );
+}
+
+if (listedCountries.length === 1) {
   return (
-    <div> 
-     <ul>{listedCountries}</ul>
-     </div>
-     )
-  }
     <>
-     
-        
-      
-  
-    
+      <p>Here is the data for </p>
+      <ul>{listedCountriesData}</ul>
+    </>
+  );
+}
 
-</>
-       
-
-
+return <div></div>;
+}
 export default Search;
-        
