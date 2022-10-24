4aa948c5c4b791defcfe800b46fdb91838eb1219
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-24 18:28:49 +0300

    reformatting again more slow, made utils file and all still works

--- part4/index.js
+++ part4/index.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose')
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog1 = require('./models/blog');
+
+//const Blog1 = require('./models/blog');
+
+
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+  const Blog = mongoose.model('Blog', blogSchema)
 const mongoUrl = process.env.MONGODB_URI //'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl)
 
