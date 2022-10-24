2bd8d57aa87f12fc6824d0ca0a602c416d895ec0
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-24 14:37:43 +0300

    working on config file seems to run but changed to PORT 3001 though

--- part4/index.js
+++ part4/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 
 const http = require('http')
 const express = require('express')
@@ -6,6 +5,10 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Blog1 = require('./models/blog');
+const config = require('./utils/config')
+
+const server = http.createServer(app)
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -15,8 +18,8 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI //'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+ //'mongodb://localhost/bloglist'
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -39,7 +42,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
