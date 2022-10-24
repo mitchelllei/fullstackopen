b1354268a4cf1e6f50b38c990a4ea610b244c34b
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-24 18:15:42 +0300

    connection works again still not posting updats its running on port 3001 instead of 3003 maybe this needs to be fixed

--- part4/app.js
+++ part4/app.js
@@ -1,16 +1,19 @@
-const http = require('http')
+//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog1 = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config')
 
 
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
  //'mongodb://localhost/bloglist'
-mongoose.connect(config.MONGODB_URI)
+//mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
