bdf7325866d3682a0e06af7371e859aad03b4388
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-25 21:50:01 +0300

    Unit test working for 0 case and for 1 case

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -6,3 +6,27 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const emptyBlog = []
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when blog is an empty list the result is 0', () => {
+    const result = listHelper.totalLikes(emptyBlog)
+    expect(result).toBe(0)
+  })
+})
