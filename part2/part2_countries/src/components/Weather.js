e81971083cc331b4c0fa300f04193592b8451623
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-30 16:14:53 +0200

    added weather file

---
+++ part2/part2_countries/src/components/Weather.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
+
+const Weather = ({ city }) => {
+  console.log("city is ",city[0])
+  const [weather, setWeather] = useState([]);
+  const [cityName, setCityName] = useState(city[0]);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+
+    async function getWeather() {
+      const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city[0]},uk&APPID=${apiKey}`
+       
+        );
+      setWeather(data);
+    }
+    // `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiKey}`
+    // `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiKey}`
+    getWeather();
+  }, [city]);
+ 
+  console.log(weather)
+  const dispWeather = weather.length !== undefined;
+  if (dispWeather) {
+    return (
+      <>
+       console.log(weather)
+        <p>nnnn</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>afasf</p>
+      </>
+    );
+  }
+};
+export default Weather
