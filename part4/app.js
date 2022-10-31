2646f8dd8e83de94bd58818d38b2d69f9dd5a7a9
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-31 11:36:05 +0200

    delete version should now be HEAD

--- part4/app.js
+++ part4/app.js
@@ -3,16 +3,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config')
+
 
 const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+ //'mongodb://localhost/bloglist'
+//mongoose.connect(config.MONGODB_URI)
+
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/blogs', blogsRouter)
-app.use(function(err,req,res,next){
-    res.status(400).send({error: err.message})
-})
 
-module.exports = app
+module.exports = app 
