daafcde05ac11fe1c08b9a2fa4cbf7f73159b32e
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-10-31 12:05:14 +0200

    seemed to have finally fixed all of these residual merge errors

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config()
 
 let PORT = process.env.PORT
@@ -11,14 +10,3 @@ module.exports = {
   MONGODB_URI,
   PORT
 }
-=======
-require('dotenv').config();
-
-const MONGODB_URI = process.env.MONGODB_URI
-const PORT = process.env.PORT
-
-module.exports = {
-    MONGODB_URI,
-    PORT
-  }
->>>>>>> b1354268a4cf1e6f50b38c990a4ea610b244c34b
