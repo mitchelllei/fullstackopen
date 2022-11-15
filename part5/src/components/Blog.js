5e90cd642cfe96b4720d0367363e784a782b1c4c
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-11-15 02:41:24 +0200

    button works to show blog details-need to reformat

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
+import React , { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [showBlogDetails, setShowBlogDetails] = useState(false)
+  const showWDetails = { show: showBlogDetails ? '' : 'none' }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonName = showBlogDetails ? 'close' : 'show'
+
+  const changeShowBlog= () => {
+    setShowBlogDetails(!showBlogDetails)
+  }
+
+  
+return(
+  <React.Fragment>
+
+
+  <div style={blogStyle}>
   <div>
-    {blog.title} {blog.author}
-  </div>  
+    <p>{blog.title} - {blog.author} <button onClick={changeShowBlog}>{buttonName}</button></p>
+  </div>
+  <div style={showWDetails }>
+    <p>{blog.url}</p>
+    <p>{ blog.likes } <button id='like' onClick={console.log("like")}>like</button></p>
+  </div>
+</div>
+  </React.Fragment>
 )
+}
+
+  
 
 export default Blog
