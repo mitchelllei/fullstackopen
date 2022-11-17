115ab3d280fb9a69c6aed333911dd1df09fcad94
Mitchell Leibowitz <mitchell@yieldsystems.tech>
2022-11-17 03:32:27 +0200

    starting author show test looking into jest.fn

---
+++ part5/src/components/Blog.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
